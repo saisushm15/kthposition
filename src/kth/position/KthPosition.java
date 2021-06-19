60139ca140515ad8c38c781a59b836d715c0d184
konda babu <konda babu@UNICORN77>
2021-06-20 00:08:21 +0530

    file

---
+++ src/kth/position/KthPosition.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kth.position;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author konda babu
+ */
+public class KthPosition
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        //int t=input.nextInt();
+       
+            String str1=input.nextLine();
+            System.out.println(str1.substring(4,5));
+            String str2=input.nextLine();
+            System.out.println(str2.substring(5,6));
+   
+    }
+}
+
+        
+        
+    
+    
+
